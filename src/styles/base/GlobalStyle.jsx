bdf6f70ad93942134b6c6ca8618152b7701f1d42
minjun <qkralswns1998@gmail.com>
2024-11-15 19:58:35 +0900

    feat 기본 스타일 정립.

--- src/styles/base/GlobalStyle.jsx
+++ src/styles/base/GlobalStyle.jsx
@@ -1,8 +1,25 @@
-import { createGlobalStyle } from "styled-components";
-import reset from "styled-reset";
+import { createGlobalStyle } from 'styled-components'
+import reset from 'styled-reset'
 
 const GlobalStyle = createGlobalStyle`
     ${reset}
+    :root {
+        --yellow: #e5b200;
+        --background: #3C3D37;
+        --orange: #eb9932;
+        --gray: #919191;
+        --khaki: #697565;
+    }
+    @font-face {
+        font-family: "Helchang";
+        src: url("/fonts/Helchang.ttf") format("truetype");
+    }
+    body {
+        background-color: var(--background);
+        font-family: "Helchang";
+        font-size: 20px;
+        color: white
+    }
 `
 
 export default GlobalStyle
