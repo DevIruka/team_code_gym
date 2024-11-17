229dcc56e5df367e4cf7faf3ca6e102267fcfe1b
ej-kimm <115481749+ej-kimm@users.noreply.github.com>
2024-11-17 20:00:02 +0900

    ✨ update : header 색상 코드 추가

--- src/styles/base/GlobalStyle.jsx
+++ src/styles/base/GlobalStyle.jsx
@@ -5,15 +5,19 @@ const GlobalStyle = createGlobalStyle`
     ${reset}
     :root {
         --yellow: #e5b200;
-        --background: #3C3D37;
         --orange: #eb9932;
+        
         --gray: #919191;
         --khaki: #697565;
+        --background: #3C3D37;
+        --header: #181C14;
     }
+
     @font-face {
         font-family: "Helchang";
         src: url("/fonts/Helchang.ttf") format("truetype");
     }
+    
     body {
         width: 100%;
         background-color: var(--background);
