defdb9ad74fa7bd511437aa227c5033da21ca013
chay140 <chaihyunpark140@gmail.com>
2024-11-18 15:00:37 +0900

    style : 마크 다운 코드 부분 최대 5줄과 줄바꿈 적용

--- src/styles/components/markdown_style/StyledMarkdown.jsx
+++ src/styles/components/markdown_style/StyledMarkdown.jsx
@@ -3,6 +3,29 @@ import styled from 'styled-components'
 const StyledMarkdown = styled.div`
   font-family: 'Fira Code', monospace;
   line-height: 1.5;
+
+  pre {
+    background-color: white;
+    padding: 10px;
+  }
+
+  pre > code {
+    text-overflow: hidden;
+    max-height: calc(1.5em * 5);
+
+    padding: 0;
+    margin: 0;
+    display: -webkit-box;
+    /* 우선 5줄로 제한 */
+    -webkit-line-clamp: 5;
+    -webkit-box-orient: vertical;
+    overflow: hidden;
+    text-overflow: ellipsis; /* ... */
+
+    // 줄 바꿈
+    white-space: pre-wrap;
+    word-break: break-all;
+  }
 `
 
 export default StyledMarkdown
