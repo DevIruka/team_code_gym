f4f4d9fd799e066eed6b49a539d94a9e4280c351
chay140 <chaihyunpark140@gmail.com>
2024-11-18 20:16:20 +0900

    add: 홈페이지에 필터링 바 영역 추가

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,9 @@
 import PostCardContainer from '../components/my_post/PostCardContainer'
+import LanguageFilterBar from '../styles/components/home_style/LanguageFilterStyle'
 const Home = () => {
   return (
     <div>
+      <LanguageFilterBar></LanguageFilterBar>
       <PostCardContainer></PostCardContainer>
     </div>
   )
---
+++ src/styles/components/home_style/LanguageFilterStyle.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+const LanguageFilterBar = styled.div`
+  background-color: lightblue;
+  width: 100%;
+  height: 3em;
+`
+
+export default LanguageFilterBar
